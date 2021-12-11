44f84d3f934c30d26eac38d9a5a029292804bb68
petchkubbb <petchkubb@live.com>
2021-12-11 19:21:45 +0700

    init project

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+const custom = require('../web/webpack.config')
+
+module.exports = {
+  stories: ['../src/components/**/*.stories.@(js|jsx|ts|tsx)'],
+  webpackFinal: (config) => {
+    return {
+      ...config,
+      resolve: { ...config.resolve, alias: { ...config.resolve.alias, ...custom.resolve.alias } },
+      module: { ...config.module, rules: custom.module.rules },
+    }
+  },
+}
---
+++ src/components/TestComponent/TestComponent.stories.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react-native';
+
+import TestComponent from '.';
+
+storiesOf('Test Component', module).add('example', () => <TestComponent />);
